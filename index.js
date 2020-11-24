c4827ddc53cd1a3b3e8113686afde8fb365dbca6
Bintang Pradana <bintangpradana02@gmail.com>
2020-11-24 13:15:23 +0700

    routing

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Home')
 })
 
 app.get('/about', (req, res) => {
   res.send('About')
+})
+
+app.get('/user', (req, res) => {
+  res.send('Get User')
+})
+
+app.post('/user', (req, res) => {
+  res.send('Post User')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Put User')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Delete User')
 })
 
 app.listen(port, () => {
