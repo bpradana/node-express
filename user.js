07b8646027e3f49ce2e6e9b6e77839498865cd2e
Bintang Pradana <bintangpradana02@gmail.com>
2020-11-24 13:59:42 +0700

    reading data

--- user.js
+++ user.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const router = express.Router()
 
+let user = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'john@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Doe',
+        email: 'doe@gmail.com'
+    }
+]
+
 router.route('/user')
   .get((req, res) => {
-    res.send('Get User')
+    res.json(user)
   })
   .post((req, res) => {
     res.send('Post User')
